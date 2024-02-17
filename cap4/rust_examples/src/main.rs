23760158b3150ccbc04bcc45ab581007dd89ebf9
Angeo Medeiros <angelomedeiros3@gmail.com>
2024-02-17 17:02:02 -0300

    feat: cap3, strings

--- cap4/rust_examples/src/main.rs
+++ cap4/rust_examples/src/main.rs
@@ -1,49 +1,78 @@
-use std::mem::replace;
-
 fn main() {
-    let mut s = vec!["joao".to_string(), "maria".to_string(), "jose".to_string()];
+    // let name = "angelo"; // &str
+    // let name = name.to_string(); // String
+    // let name = name.as_str(); // &str
 
-    if s[0].len() == 3 {
-        dbg(s);
-        s = [].to_vec(); // caso nao seja "inicializado" novamente ocorrera erro depois do if
-    } else {
-        dbg(s);
-        s = [].to_vec(); // caso nao seja "inicializado" novamente ocorrera erro depois do if
-    }
+    // println!("{}", name.replace("a", "A")); // String
 
-    dbg(s); // nao aprensenta erro pois s foi inicializado novamente dentro do if
+    // let mut s = "angelo".to_string(); // String
+    // s.push_str(" medeiros"); // String
 
-    let v = vec!["joao".to_string(), "maria".to_string(), "jose".to_string()];
+    // println!("{}", s);
 
-    for mut i in v {
-        i.push('!');
-        println!("{}", i);
-    }
+    // let mut s = vec!["joao".to_string(), "maria".to_string(), "jose".to_string()];
 
-    #[derive(Debug)]
-    struct Person {
-        name: Option<String>,
-        birth: i32,
-    }
+    // if s[0].len() == 3 {
+    //     dbg(s);
+    //     s = [].to_vec(); // caso nao seja "inicializado" novamente ocorrera erro depois do if
+    // } else {
+    //     dbg(s);
+    //     s = [].to_vec(); // caso nao seja "inicializado" novamente ocorrera erro depois do if
+    // }
 
-    let mut composers = Vec::new();
-    composers.push(Person {
-        name: Some("Angelo Medeiros".to_string()),
-        birth: 1995,
-    });
+    // dbg(s); // nao aprensenta erro pois s foi inicializado novamente dentro do if
 
-    // let first_name = composers[0].name; //cannot move out of index of `Vec<Person>`
-    let first_name = replace(&mut composers[0].name, None);
-    // poderia ser usado composers[0].name.take() na linha de cima
-    // ex: let first_name = composers[0].name.take();
-    assert_eq!(first_name, Some("Angelo Medeiros".to_string()));
+    // let v = vec!["joao".to_string(), "maria".to_string(), "jose".to_string()];
 
-    let none_name = composers[0].name.take();
-    assert_eq!(none_name, None);
+    // for mut i in v {
+    //     i.push('!');
+    //     println!("{}", i);
+    // }
+
+    // #[derive(Debug)]
+    // struct Person {
+    //     name: Option<String>,
+    //     birth: i32,
+    // }
+
+    // let mut composers = Vec::new();
+    // composers.push(Person {
+    //     name: Some("Angelo Medeiros".to_string()),
+    //     birth: 1995,
+    // });
+
+    // // let first_name = composers[0].name; //cannot move out of index of `Vec<Person>`
+    // let first_name = replace(&mut composers[0].name, None);
+    // // poderia ser usado composers[0].name.take() na linha de cima
+    // // ex: let first_name = composers[0].name.take();
+    // assert_eq!(first_name, Some("Angelo Medeiros".to_string()));
+
+    // let none_name = composers[0].name.take();
+    // assert_eq!(none_name, None);
+
+    // println!(
+    //     r##"
+    //         Teste "raw string"
+    //     "##
+    // );
+
+    // let method = b"GET"; // &[u8; 3]
+    // println!("{:?}", method);
+
+    play_with_strings();
 }
 
 fn dbg<T: std::fmt::Debug>(s: Vec<T>) {
     println!("==== Value ====");
     dbg!(s);
     println!("==== End ====");
+}
+
+fn play_with_strings() {
+    let noodles = "noodles".to_string(); // String
+    let oodles = &noodles[1..]; // &str
+    let poodles = "ಠ_ಠ".to_string(); // String
+
+    println!("{}", poodles);
+    println!("{}", oodles);
 }
