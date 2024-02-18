8b81322375f714add4d514b0b5a502e8e5deca9c
Angeo Medeiros <angelomedeiros3@gmail.com>
2024-02-18 14:26:34 -0300

    feat: cap5, references, string slice

--- cap5/references/src/main.rs
+++ cap5/references/src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
-    let s = String::from("hello world");
-    let hello = &s[0..5];
-    let world = &s[6..];
+    let mut s = String::from("hello world");
+    // let hello = &s[0..5];
+    // let world = &s[6..];
 
-    println!("{} {}!", hello, world);
+    let word = first_word(&s[0..6]);
+    println!("{}", &s[..]);
+
+    // s.push('!'); // error!
+
+    println!("the first word is: {}", word);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
