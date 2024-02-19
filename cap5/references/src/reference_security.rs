4fd904bf97642904b83dd4b8a4cb0e005781dc9a
Angeo Medeiros <angelomedeiros3@gmail.com>
2024-02-19 01:36:13 -0300

    feat: cap5, reference security

--- cap5/references/src/reference_security.rs
+++ cap5/references/src/reference_security.rs
@@ -8,4 +8,21 @@ pub fn reference_security() {
     }
 
     // assert_eq!(*r, 1); // ruim: lê a memoria que x ocupava
+    static mut WORTH_POINTING_AT: &i32 = &666;
+
+    unsafe {
+        f(&WORTH_POINTING_AT);
+    }
+
+    unsafe {
+        println!("STASH: {}", STASH);
+    }
+}
+
+static mut STASH: &i32 = &123;
+
+fn f(p: &'static i32) {
+    unsafe {
+        STASH = p;
+    }
 }
