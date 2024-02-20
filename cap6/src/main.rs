3a337d9865e24ef97c8b51e10fa6412db1f1fea9
Angeo Medeiros <angelomedeiros3@gmail.com>
2024-02-19 22:59:35 -0300

    feat: cap6, code block

--- cap6/src/main.rs
+++ cap6/src/main.rs
@@ -1,9 +1,28 @@
 fn main() {
+    // Statements
     let msg = {
         let name = "angelo".to_string();
 
-        name
+        name // return value to variable
     };
 
     println!("Hello, world, {}!", msg);
+
+    // Code block
+    fn show_files() {
+        let names = vec!["Maria".to_string(), "Gabriel".to_string()];
+
+        fn print_vec(vector: &Vec<String>) {
+            // dbg!(&names); // Cannot access names outside of a nested function
+            for v in vector {
+                println!("{}", v);
+            }
+        }
+
+        print_vec(&names);
+    }
+
+    show_files();
+
+    //
 }
