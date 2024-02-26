728d2ecdd53657eb129b87a1199aeb9acad2a606
Angeo Medeiros <angelomedeiros3@gmail.com>
2024-02-26 00:49:30 -0300

    refactor: cap7, using unrap_or_else

--- cap7/src/main.rs
+++ cap7/src/main.rs
@@ -8,20 +8,31 @@ fn main() {
 
     print_error(&err2);
 
-    let greeting_file_result = File::open("hello.txt");
+    // let greeting_file_result = File::open("hello.txt");
 
-    let mut greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(error) => panic!("Problem opening the file: {:?}", error),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error)
-            }
-        },
-    };
+    // let mut greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(error) => panic!("Problem opening the file: {:?}", error),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
+
+    let mut greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
     let mut contents = String::new();
     let _ = greeting_file.read_to_string(&mut contents);
 
