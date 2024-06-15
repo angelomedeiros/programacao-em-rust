23e9114333fd9f4417f2f8d41a98d6f48c1d97e1
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-06-15 15:11:53 -0300

    feat: main criando um array novo

--- rust_playground/src/main.rs
+++ rust_playground/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     bubble_sort(&mut array);
     println!("Array ordenado pelo bubble sort: {:?}", array);
 
-    let sorted_by_selection = selection_sort(&mut array);
+    let mut array2 = [64, 55, 25, 12, 13, 9, 90];
+    let sorted_by_selection = selection_sort(&mut array2);
     println!(
         "Array ordenado pelo selection sort: {:?}",
         sorted_by_selection
